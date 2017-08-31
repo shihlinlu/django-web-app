1e6b39e808fd818c877154ac5a9600cf4a4dcc58
Shihlin Lu <shihlinlu@gmail.com>
2017-08-31 12:50:33 -0600

    added template views, moved context, removed previous views

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,36 +1,32 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 import random
 
-from django.shortcuts import render
+class HomeView(TemplateView):
+    template_name = 'home.html'
 
-def home(request):
-    num = None
-    some_list = [
-        random.randint(0, 10000),
-        random.randint(0, 10000),
-        random.randint(0, 10000)
-    ]
-    condition_bool_item = True
-    if condition_bool_item:
-        num = random.randint(0, 10000)
-    context = {
-        "num": num,
-        "some_list": some_list
-    }
-    return render(request, "home.html", context)
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        num = None
+        some_list = [
+            random.randint(0, 10000),
+            random.randint(0, 10000),
+            random.randint(0, 10000)
+        ]
+        condition_bool_item = True
+        if condition_bool_item:
+            num = random.randint(0, 10000)
+        context = {
+            "num": num,
+            "some_list": some_list
+        }
+        return context
 
-def about(request):
-    context = {
+class AboutView(TemplateView):
+    template_name = 'about.html'
 
-    }
-    return render(request, "about.html", context)
-
-
-def contact(request):
-    context = {
-
-    }
-    return render(request, "contact.html", context)
-
+class ContactView(TemplateView):
+    template_name = 'contact.html'
