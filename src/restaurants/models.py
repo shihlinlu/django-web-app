6a10d326c389a6f5e8e46ac91b76f944154e6705
Shihlin Lu <shihlinlu@gmail.com>
2017-08-31 13:36:58 -0600

    added restaurantlocation model

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -3,4 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class RestaurantLocation(models.Model):
+    name = models.CharField(max_length=120)
+    location = models.CharField(max_length=120, null=True, blank=True)
