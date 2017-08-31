d2f24f5cd8d6d87e9835a8ca1c0b0bdf88b213de
Shihlin Lu <shihlinlu@gmail.com>
2017-08-31 15:07:29 -0600

    added restaurant list template to display saved object data

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -3,30 +3,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
-import random
 
-class HomeView(TemplateView):
-    template_name = 'home.html'
+from .models import RestaurantLocation
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(HomeView, self).get_context_data(*args, **kwargs)
-        num = None
-        some_list = [
-            random.randint(0, 10000),
-            random.randint(0, 10000),
-            random.randint(0, 10000)
-        ]
-        condition_bool_item = True
-        if condition_bool_item:
-            num = random.randint(0, 10000)
-        context = {
-            "num": num,
-            "some_list": some_list
-        }
-        return context
-
-class AboutView(TemplateView):
-    template_name = 'about.html'
-
-class ContactView(TemplateView):
-    template_name = 'contact.html'
+def restaurant_listview(request):
+    template_name = 'restaurants/restaurant_list.html'
+    queryset = RestaurantLocation.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, template_name, context)
