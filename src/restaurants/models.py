5c7f0993d296b6110a9c36a5e8e766dcabbc8068
Shihlin Lu <shihlinlu@gmail.com>
2017-09-07 14:55:57 -0600

    added signal and unique slug generator

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.signals import pre_save, post_save\
+
+from .utils import unique_slug_generator
 
 class RestaurantLocation(models.Model):
     name = models.CharField(max_length=120)
@@ -10,7 +13,29 @@ class RestaurantLocation(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     my_date_field = models.DateTimeField(auto_now=False, auto_now_add=False)
+    slug = models.SlugField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def title(self):
+        return self.name
+
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+# def rl_post_save_receiver(sender, instance, created, *args, **kwargs):
+#     print('saved')
+#     print(instance.timestamp)
+#     if not instance.slug:
+#         instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(rl_pre_save_receiver, sender=RestaurantLocation)
+
+# post_save.connect(rl_post_save_receiver, sender=RestaurantLocation)
+
+
+
 
